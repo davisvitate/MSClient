f2562713ad99d80e7f5174c1bfd6781d16e611e7
davisvitate <davisvitate@gmail.com>
2019-12-05 18:49:34 -0500

    implementando los atributos de client

--- src/main/java/com/microservice/client/model/Clients.java
+++ src/main/java/com/microservice/client/model/Clients.java
@@ -1,9 +1,22 @@
 package com.microservice.client.model;
 
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+
+@Data
+@Document(collection = "clients")
 public class Clients {
-	
+@NotBlank	
 private String id;
+@NotBlank	
 private String name;
+@NotBlank	
 private String lastName;
-private String 
+@DateTimeFormat(pattern = "yyyy-MM-dd")
+private Date dateBir;
 }
