cfb02f03d850db7bcef77be4cd3d3331092d0273
davisvitate <davisvitate@gmail.com>
2019-12-05 18:25:02 -0500

    modificando el nombre de client en Client

---
+++ src/main/java/com/microservice/client/model/Clients.java
@@ -0,0 +1,9 @@
+package com.microservice.client.model;
+
+public class Clients {
+	
+private String id;
+private String name;
+private String lastName;
+private String 
+}
--- src/main/java/com/microservice/client/model/clients.java
+++
@@ -1,5 +0,0 @@
-package com.microservice.client.model;
-
-public class clients {
-
-}
